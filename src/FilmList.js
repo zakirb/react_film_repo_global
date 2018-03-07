853e51c0aabe1d5fd85b1c8cf53a94f097364470
Zakir Butte <zakir.butte@gmailcom>
2018-03-07 15:28:24 -0800

    added class switches

--- src/FilmList.js
+++ src/FilmList.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 import FilmRow from './FilmRow';
 
 class FilmList extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      filter: 'all'
+    }
+  }
 
   handleFilterClick = (filter) => {
     console.log('Setting filter to ' + filter)
+    this.setState({
+      filter: filter
+    })
   }
 
 
@@ -28,10 +37,10 @@ class FilmList extends Component {
       <div className="film-list">
         <h1 className="section-title">FILMS</h1>
         <div className="film-list-filters">
-          <div className="film-list-filter" onClick={(e) => this.handleFilterClick('all')}>
+          <div className="film-list-filter {this.state.filter === 'all' ? 'is-active' : ''}" onClick={(e) => this.handleFilterClick('all')}>
           ALL<span className="section-count">{this.props.films.length}</span>
           </div>
-        <div className="film-list-filter" onClick={(e) => this.handleFilterClick('faves')}>
+        <div className="film-list-filter {this.state.filter === 'faves' ? 'is-active' : ''}" onClick={(e) => this.handleFilterClick('faves')}>
         FAVES<span className="section-count">0</span>
         </div>
       </div>

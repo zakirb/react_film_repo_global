f0855df64f0b46b553460da2d01822e912352b7d
Zakir Butte <zakir.butte@gmailcom>
2018-03-13 13:21:42 -0700

    refactored components and added details display

--- src/FilmDetails.js
+++ src/FilmDetails.js
@@ -1,16 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 
-class FilmDetails extends Component {
-  render() {
+const FilmDetails = props => {
+
+  const backdropUrl = `https://image.tmdb.org/t/p/w1280/${props.film.backdrop_path}`
+  const posterUrl = `https://image.tmdb.org/t/p/w780/${props.film.poster_path}`
 
 
-    return (
-      <div className="film-details">
-        <h1 className="section-title">DETAILS</h1>
+  if (props.film.id) {
+    var detail = (
+      <div className="film-detail is-hydrated">
+        <figure className="film-backdrop">
+          <img src={backdropUrl} alt="" />
+          <h1 className="film-title">{props.film.title}</h1>
+        </figure>
+
+        <div className="film-meta">
+          <h2 className="film-tagline">{props.film.tagline}</h2>
+          <p className="film-detail-overview">
+            <img src={posterUrl} className="film-detail-poster" alt={props.film.title} />
+            {props.film.overview}
+          </p>
+        </div>
       </div>
     )
-  }
+  } else {
+      var detail = (
+        <div className="film-detail">
+          <p>
+            <i className="material-icons">subscriptions</i>
+            <span>No film selected</span>
+          </p>
+        </div>
+      )
+    }
+  return (
+    <div className="film-details">
+      <h1 className="section-title">DETAILS</h1>
+      {detail}
+    </div>
+  )
 }
 
 export default FilmDetails
--- src/FilmRow.js
+++ src/FilmRow.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import FilmPoster from './FilmPoster';
 import Fave from './Fave';
 
-class FilmRow extends Component {
+const FilmRow = props => {
 
-
-  render() {
-
-    const {title, date, poster_url} = this.props
+    const {title, date, poster_url, onDetailsClick, onFaveToggle, isFave} = props
 
     let posterUrl = "https://image.tmdb.org/t/p/w780/" + poster_url;
 
     let year = new Date(date).getFullYear();
 
     return (
-      <div className="film-row" onClick={this.props.onDetailsClick}>
+      <div className="film-row" onClick={onDetailsClick}>
         <FilmPoster url={posterUrl} title={title}/>
         <div className="film-summary">
           <h1>{title}</h1>
           <p>{year}</p>
         </div>
-        <Fave isFave={this.props.isFave} onFaveToggle={this.props.onFaveToggle} />
+        <Fave isFave={isFave} onFaveToggle={onFaveToggle} />
       </div>
     )
-  }
 }
 export default FilmRow
